77b1a80800f3c548c425aaa120314e6064e7cd8c
Laura <laurajoyhutchins@gmail.com>
2026-07-15 09:21:33 -0600

    feat: export Python artifact helpers

--- packages/worker-sdk-py/src/factory_floor_worker_sdk/__init__.py
+++ packages/worker-sdk-py/src/factory_floor_worker_sdk/__init__.py
@@ -1,4 +1,38 @@
 """Factory Floor Python worker SDK."""
+
 __version__ = "0.1.0"
-from .client import *
+
+from .artifacts import stage_bytes, stage_json
+from .client import (
+    PROTOCOL_VERSION,
+    ConflictingResultError,
+    ProtocolError,
+    ProtocolValidationError,
+    TransportError,
+    WorkerClient,
+    WorkerClientConfig,
+    WorkerSdkError,
+    canonical_json_bytes,
+    digest_bytes,
+    redact,
+)
 from .runner import WorkerComponent, WorkerContext, WorkerRunner
+
+__all__ = [
+    "PROTOCOL_VERSION",
+    "ConflictingResultError",
+    "ProtocolError",
+    "ProtocolValidationError",
+    "TransportError",
+    "WorkerClient",
+    "WorkerClientConfig",
+    "WorkerComponent",
+    "WorkerContext",
+    "WorkerRunner",
+    "WorkerSdkError",
+    "canonical_json_bytes",
+    "digest_bytes",
+    "redact",
+    "stage_bytes",
+    "stage_json",
+]
